223bd029e4f993071064dc5240dc40af0b2ed0e9
d-wack <dotwack@pm.me>
2025-03-20 20:24:17 +0200

    New Changed

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Amplify } from 'aws-amplify';
+import output from '../amplify_outputs.json';
+
+Amplify.configure(output);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
